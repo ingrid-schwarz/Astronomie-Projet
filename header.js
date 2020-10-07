200ceb98aa51bcb992b73b3eceda8727159bf3f6
ingrid-schwarz <ingrid.schwarz.wcs@gmail.com>
2020-10-07 11:54:00 +0200

    css menu burger

--- header.js
+++ header.js
@@ -1,8 +1,6 @@
 let b_btn =document.querySelector("button.b-btn");
-b_btn.addEventListener('click', function{
+b_btn.addEventListener('click', function(){
     document.querySelectorAll("nav.burger").style.display="block";    
 })
-if(screen.width < "721px"){
-    burger.style.display = "block";  
-}
+
 
